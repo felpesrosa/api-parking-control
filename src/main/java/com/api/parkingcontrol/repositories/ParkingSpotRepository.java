694a51d9f048c0551d08975a612649850b72de30
Felipe <felpesrosa@gmail.com>
2022-02-08 23:15:05 -0300

    Criação do projeto e dos métodos de CRUD

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,19 @@
+package com.api.parkingcontrol.repositories;
+
+import java.util.UUID;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+
+    boolean existsByParkingSpotNumber(String parkingSpotNumbe);
+
+    boolean existsByApartmentAndBlock(String apartment, String block);
+    
+}
